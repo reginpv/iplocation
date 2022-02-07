3cf5aa3027b8c44883ed00f542900f4f170076e3
reginpv <reginpv@gmail.com>
2022-02-07 15:15:15 +0800

    update

--- pages/api/ip.js
+++ pages/api/ip.js
@@ -2,19 +2,11 @@ import countries from '../lib/countries.json'
 
 export default function handler(req, res) {
 
-  console.log(req.qeo)
+  console.log('query: ',req.query)
 
-  const { url, geo } = req
-  const country = geo?.country || 'na'
-  const city = geo?.city || 'na'
-  const region = geo?.region || 'na'
 
-  let payload = {
-    country,
-    city,
-    region,
-    geo: req?.geo
-  }
+
+  let payload = req.query
 
   res.status(200).json(payload)
 }
